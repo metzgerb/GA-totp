bccff6cbb7dc5e0688a8509ed27f377963d3bbf0
metzgerb <40550164+metzgerb@users.noreply.github.com>
2019-11-29 11:53:53 -0600

    Update submission.py

--- submission.py
+++ submission.py
@@ -10,27 +10,71 @@ Created: 2019-11-25
 Last Modified: 2019-11-25
 """
 
-import sys, os
+import sys, os, qrcode, time, urllib.parse, base64
+
+"""
+Function Name: totp
+Description: generates a time-based on-time-password
+Inputs: a string representing the secret key.
+Outputs: string containing the password
+"""
+def totp(secret):
+    
+    #get current time
+    current_time = time.time()
+    
+    #get time step by dividing current time since epoch by 30 seconds using floor division
+    time_step = current_time//30
+    
+    #generate SHA-1 hash
+    
+    
+    #truncate hash to 6 digits
+    
+    
+    return str(time_step)
 
 """
 Function Name: generate_qr
 Description: generates an image file with a QR code to be used with Google
 Authenticator. The QR code is an encoded URI.
-Inputs: nothing
-Outputs: svg file containing a QR code
+Inputs: a string representing an account, a string representing the issuer,
+a string representing the secret key.
+Outputs: JPG file containing a QR code
 """
-def generate_qr():
-    print("generate_qr")
+def generate_qr(account, issuer, secret):
+    #build initial uri string for QR encoding
+    uri_string = "//totp/"
+    
+    #add issuer prefix for label
+    uri_string += urllib.parse.quote(issuer)
+    uri_string += ":"
+    
+    #add account for label
+    uri_string += urllib.parse.quote(account)
+    
+    #add base32 encoded secret key for parameters
+    uri_string += "?secret="
+    uri_string += base64.b32encode(secret)
+    
+    
+    
+    print(uri_string)
+    #generate QR data as image
+    img = qrcode.make('Some data here')
+    
+    #save img
+    img.save("qr.jpg")
 
 
 """
 Function Name: get_otp
 Description: generates an OTP that matches Google Authenticator's OTP for each
 30-second period. This repeats every thirty seconds until interrupted. 
-Inputs: nothing
+Inputs: a string representing the secret key.
 Outputs: outputs an OTP every 30 seconds to stdout
 """
-def get_otp():
+def get_otp(secret):
     print("get_otp")
 
 
@@ -41,6 +85,11 @@ Inputs: takes 1 argument (submission.py --command)
 Outputs: returns nothing
 """
 if __name__ == "__main__":
+    #set configs
+    account = "example@gmail.com"
+    issuer = "example co"
+    secret = "this is a test"
+    
     #check total argument count
     if len(sys.argv) != 2:
         print("USAGE: %s --command" % sys.argv[0])
@@ -50,6 +99,6 @@ if __name__ == "__main__":
     #everything is ok, call main function
     else:
         if sys.argv[1] == "--generate-qr":
-            generate_qr()
+            generate_qr(account, issuer, secret)
         else:
-            get_otp()
+            get_otp(secret)
