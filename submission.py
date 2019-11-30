dcb6197f18950dcc2d9ec6a7d77415eb62c9c102
metzgerb <40550164+metzgerb@users.noreply.github.com>
2019-11-30 08:09:56 -0600

    Update submission.py
    added code calculations

--- submission.py
+++ submission.py
@@ -53,27 +53,39 @@ Function Name: get_otp
 Description: generates an OTP that matches Google Authenticator's OTP for each
 30-second period. This repeats every thirty seconds until interrupted. 
 Inputs: a string representing the secret key.
-Outputs: outputs an OTP every 30 seconds to stdout
+Outputs: outputs an TOTP every 30 seconds to stdout
 """
 def get_otp(secret):
-    #get time step by dividing current time since epoch by 30 seconds using floor division
-    time_step = int(time.time())//30
+    #loop until interrupted by keybord
+    while(True):
+        #get time step by dividing current time since epoch by 30 seconds using floor division
+        time_step = int(time.time())//30
     
-    #convert key and timestep to bytes
-    key = bytes(secret, "utf-8")
-    time_interval = time_step.to_bytes(8, byteorder="big")
+        #convert key and timestep to bytes
+        key = bytes(secret, "utf-8")
+        time_interval = time_step.to_bytes(8, byteorder="big")
     
-    #generate SHA-1 hash
-    hash = hmac.new(key, time_interval, hashlib.sha1).hexdigest()
-    print(hash)
+        #generate SHA-1 hash
+        hash = hmac.new(key, time_interval, hashlib.sha1).hexdigest()
     
-    #determine offset
-    print(int(hash[-1],16))
+        #determine offset from the lower bit order of the last byte
+        offset = int(hash[-1],16)
     
-    #get last 4 bytes of hash
-    #truncate hash to 6 digits
+        byte_hash = bytes.fromhex(hash)
+        #get 4 bytes of hash starting at offset
+        bit_code = (byte_hash[offset] & 0x7f) << 24 | (byte_hash[offset + 1] & 0xff) << 16 | (byte_hash[offset + 2] & 0xff) << 8 |(byte_hash[offset + 3] & 0xff)
+ 
+        #truncate hash bytes to 6 digits and pad with 0s
+        auth_code = str(bit_code % 1000000).zfill(6)
     
+        #print with formatting space between code chunks
+        print(auth_code[:3] + " " + auth_code[-3:])
     
+        #calculate sleep interval (next time step in seconds - current time)
+        next_time = time_step + 1
+        
+        #sleep using difference of next time step and current time
+        time.sleep((next_time*30) - time.time())
 
 
 """
